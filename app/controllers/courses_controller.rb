2f24944fb8cdfb9eb1e22182ea8aa26601e14df2
dkersten <dkersten1994@gmail.com>
2020-06-29 17:37:28 -0400

    added show action for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -3,4 +3,11 @@ class CoursesController < ApplicationController
         courses = Course.all
         render json: courses.to_json
     end
+
+    def show
+        course = Course.find(params[:id])
+        render json: course.to_json(:include => {
+            :holes => {:only => [:name, :picture, :par, :id, :distance]}
+            }, except: [:created_at, :updated_at])
+    end
 end
