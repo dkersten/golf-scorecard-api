066256686270d5d24758acced626da182629b092
dkersten <dkersten1994@gmail.com>
2020-07-05 23:02:58 -0400

    user auth working

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,1 +1,14 @@
-class AuthController
+class AuthController < ApplicationController
+
+    skip_before_action :verify_authenticity_token
+
+    def login
+        user = User.find_by(email: params[:email])
+        if (user && user.authenticate(params[:password]))
+            render json: user, only: [:id, :email, :first_name, :last_name]
+        else
+            render json: {errors: "invalid username or password"}
+        end
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :holes, only: [:index]
   resources :users
   resources :scorecards
+
+  post '/login', to: 'auth#login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
